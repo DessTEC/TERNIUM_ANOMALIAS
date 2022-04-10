66e2bc80c0b3c23796a4382eab102c90261331fc
itumejia <iturielmg@hotmail.com>
2022-04-10 12:06:10 -0500

    Se agregan botones filtro/descarga

--- src/components/TablaScreen/TablaScreen.js
+++ src/components/TablaScreen/TablaScreen.js
@@ -1,8 +1,13 @@
+import "./TablaScreen.css"
+
 export const TablaScreen = () => {
 
     return(
-        <>
-           <h1>Tabla</h1>
-        </>
+        <div>
+            <div className="container-buttons">
+            <button className="btn btn-primary">Filtros</button>
+            <button className="btn btn-outline-primary">Descargar</button>
+            </div>
+        </div>
     );
 }
