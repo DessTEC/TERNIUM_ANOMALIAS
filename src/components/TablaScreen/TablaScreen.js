17001a72aee3aeb1ae94d37cae3f0263ea92d473
itumejia <iturielmg@hotmail.com>
2022-04-10 19:11:58 -0500

    Iconos en los botones Filtro/Descarga

--- src/components/TablaScreen/TablaScreen.js
+++ src/components/TablaScreen/TablaScreen.js
@@ -1,13 +1,22 @@
 import "./TablaScreen.css"
 import Tabla from "./Tabla";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFilter } from "@fortawesome/free-solid-svg-icons";
+import { faFileArrowDown } from "@fortawesome/free-solid-svg-icons";
 
 export const TablaScreen = () => {
 
     return(
         <div>
             <div className="container-buttons">
-                <button className="btn btn-primary">Filtros</button>
-                <button className="btn btn-outline-primary">Descargar</button>
+                <button className="btn btn-primary">
+                    <FontAwesomeIcon icon={faFilter} className="buttonIcon"/>
+                    Filtros
+                </button>
+                <button className="btn btn-outline-primary">
+                    <FontAwesomeIcon icon={faFileArrowDown} className="buttonIcon"/>
+                    Descargar
+                </button>
             </div>
             <Tabla/>
         </div>
