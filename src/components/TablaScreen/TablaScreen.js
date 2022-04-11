81519d6c2dde68d1b6d2cc368b9bc53c391c2463
itumejia <iturielmg@hotmail.com>
2022-04-10 21:15:52 -0500

    pantalla de seleccion de parametros

--- src/components/TablaScreen/TablaScreen.js
+++ src/components/TablaScreen/TablaScreen.js
@@ -1,5 +1,5 @@
 import "./TablaScreen.css"
-import Tabla from "./Tabla";
+import Tabla from "../tabla/Tabla";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFilter } from "@fortawesome/free-solid-svg-icons";
 import { faFileArrowDown } from "@fortawesome/free-solid-svg-icons";
@@ -18,7 +18,7 @@ export const TablaScreen = () => {
                     Descargar
                 </button>
             </div>
-            <Tabla/>
+            <Tabla hasCheckboxes={false}/>
         </div>
     );
 }
