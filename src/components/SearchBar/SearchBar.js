bc086e82cfd8409bd0266d0aacce66448b3d0a47
adriabellak <adriabellak@gmail.com>
2022-04-16 23:14:49 -0500

    Primera pantalla de consulta

---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,17 @@
+export const SearchBar = () => (
+    <form action="/" method="get">
+        <label htmlFor="header-search">
+            <span className="visually-hidden">Buscar</span>
+        </label>
+        <input
+            type="text"
+            id="header-search"
+            placeholder="Buscar por nombre"
+            name="s" 
+            className='bg-[#F2F2F2] rounded-xl pt-2 pb-2 pl-3 w-1/2'
+        />
+        {/* <button type="submit">Search</button> */}
+    </form>
+);
+
+// export default SearchBar;
