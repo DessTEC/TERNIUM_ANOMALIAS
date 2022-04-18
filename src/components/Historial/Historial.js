42cc2df567d5d7674c175cd451d8c0efe6506983
adriabellak <adriabellak@gmail.com>
2022-04-17 23:26:17 -0500

    Navegar de historial a modelos

--- src/components/Historial/Historial.js
+++ src/components/Historial/Historial.js
@@ -4,22 +4,27 @@ import industria from '../../assets/industria.png';
 import { SearchBar } from '../SearchBar/SearchBar';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCloudArrowUp } from '@fortawesome/free-solid-svg-icons'
+import { faFilter } from "@fortawesome/free-solid-svg-icons";
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 
 export const Historial = () => {
 
-    const name1 = 'semestre2019';
+    const name1 = 'Primer Trimestre';
     const name2 = 'semestre2020';
 
     return (
           <div>
             <div className='max-w-[1240px] mx-auto text-black relative mb-10'>
                 <div className='grid grid-cols-1 lg:grid-cols-1 relative gap-y-10 px-4 pt-20 justify-items-center'>
-                    <div className='p-4 w-full'>
-                            <p className='text-black font-bold text-center text-xl'>CONSULTAR</p>
-                            <p className='text-black font-normal text-center text-xl pb-3'>Navega por los reportes generados anteriormente</p>
-                            <SearchBar />
+                    <div className='w-full'>
+                        <button className="btn btn-primary bg-[#F25C29] border-0 hover:bg-[#D15226]">
+                            <FontAwesomeIcon icon={faFilter} className="buttonIcon pr-2"/>
+                            Filtros
+                        </button>
+                        <p className='text-black font-bold text-center text-xl'>CONSULTAR</p>
+                        <p className='text-black font-normal text-center text-xl pb-3'>Navega por los reportes generados anteriormente</p>
+                        <SearchBar />
                     </div>
                     <Link to={name1} className='bg-[#F3F6FF] rounded-t-xl rounded-b-xl pb-2 w-2/3'>
                         <div className='p-4 w-full'>
--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,14 +1,11 @@
 export const SearchBar = () => (
-    <form action="/" method="get">
-        <label htmlFor="header-search">
-            <span className="visually-hidden">Buscar</span>
-        </label>
+    <form action="/" method="get" className='text-center'>
         <input
             type="text"
             id="header-search"
             placeholder="Buscar por nombre"
             name="s" 
-            className='bg-[#F2F2F2] rounded-xl pt-2 pb-2 pl-3 w-1/2'
+            className='bg-[#F2F2F2] rounded-xl pt-2 pb-2 pl-3 w-1/3'
         />
         {/* <button type="submit">Search</button> */}
     </form>
