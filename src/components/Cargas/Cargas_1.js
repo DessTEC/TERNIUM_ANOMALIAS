17963bb054845beef5598032c3200851fbd241ad
Ana Fernanda <A01411484@itesm.mx>
2022-04-27 22:41:27 -0500

    Actualizacion pagina (sin funcion)

--- src/components/Cargas/Cargas_1.js
+++ src/components/Cargas/Cargas_1.js
@@ -4,27 +4,38 @@ import './Cargas.css'
 export const Cargas_1 = () => {
 
     return(
-    <><div className="">
-            <h2 className="titulo"> CARGAR DATOS</h2>
-            <p className="titulo"> Confirmar la configuración del archivo</p>
-        </div><div className="divrec">
-                <div className="divfon">
-                    <p> Archivo a cargar</p>
-                </div>
+        <><div className="">
+        <h2 className="titulo"> CARGAR DATOS</h2>
+        <p className="titulo"> Confirmar la configuración del archivo</p>
+    </div><div className="divrec">
+            <div className="divfon">
+            <p align = "right"> 
+            <p align = "left"> Archivo a cargar </p>
+                <button className='botonedit'> Edit</button>
+                </p>
+                
+            </div>
 
-                <div className="divfon2">
-                    Parametros de analisis
-                </div>
+            <div className="divfon2">
+            <p align = "right"> 
+            <p align = "left"> Parametros de analisis </p>
+                <button className='botonedit'> Edit</button>
+                </p>
+            </div>
 
-                <h2 className="positionlabeltable">Nombre de Archivo:
+            <h2 className="positionlabeltable">Nombre de Archivo:
 
-                </h2>
-                <div className="ingresanombre">
-                </div>
-            </div><div className="circlecarga">
-                <img src={lupa} alt="sample"/>
-                </div>
-                </> 
+            </h2>
+            <div className="ingresanombre">
+              <input className='textbox'></input>
+            </div>
+        </div><div >
+            
+            <button className='circlecarga'>
+            <img src={lupa} alt="sample"/>
+            </button>
+            </div>
+            </> 
 
     )
 }
