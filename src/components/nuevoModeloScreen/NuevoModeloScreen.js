040e23489a431de5951ae15bd193bc8f65f819b8
itumejia <iturielmg@hotmail.com>
2022-05-03 19:23:19 -0500

    fixes nuevoModelo screen

--- src/components/nuevoModeloScreen/NuevoModeloScreen.js
+++ src/components/nuevoModeloScreen/NuevoModeloScreen.js
@@ -1,7 +1,9 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import Tabla from "../tabla/Tabla";
 import "./NuevoModeloScreen.css"
 import axios from "axios";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faXmark } from "@fortawesome/free-solid-svg-icons";
 
 import { useParams, useNavigate } from "react-router-dom";
 
@@ -11,9 +13,11 @@ export const NuevoModeloScreen = () => {
     const navigate = useNavigate();
 
     const [dataCsv, setDataCsv] = useState();
+    var resultRequest = useRef([]);
     const [atributos, setAtributos] = useState(undefined);
     const params = useParams();
     const [selectedVars, setSelectedVars] = useState([]);
+    const [emptiedFilters, setEmptiedFilters] = useState(true);
 
     const [reporteId, setReporteId] = useState(params.reporteId);
     const [modeloId, setModeloId] = useState(undefined);
@@ -26,7 +30,8 @@ export const NuevoModeloScreen = () => {
         const result = await axios.get(
             "http://localhost:4000/getReporteById", {params: {id: reporteId}}, 
         );
-        setDataCsv(result["data"]["data"]);
+        resultRequest.current = result["data"]["data"];
+        setDataCsv(resultRequest.current.map((item) => item));
         setAtributos([...result["data"]["actoresInternos"], ...result["data"]["actoresExternos"]]);
     }
 
@@ -57,18 +62,27 @@ export const NuevoModeloScreen = () => {
     const handleInputChange = (e) => {
         setInputValue(e.target.value);
     }
+
+    function eliminarFiltros() {
+        setDataCsv(resultRequest.current);
+        setEmptiedFilters(true);
+    }
     
 
     return(
         <div className='main'>
             <div className="header-screen">
+                <button className="btn btn-secondary eliminar-filtros-button" onClick={eliminarFiltros}>
+                    <FontAwesomeIcon icon={faXmark} className="buttonIcon"/>
+                    Eliminar Filtros
+                </button>
                 <h1>SELECCIONAR PARÁMETROS</h1>
                 <button className="btn btn-outline-primary aplicar-modelo" onClick={createModelo}>Aplicar Modelo</button>
             </div>
             {
                 atributos === undefined ? <></> :
                 <>
-                    <Tabla hasCheckboxes={true} setSelectedVars={setSelectedVars} data={dataCsv} atributos={atributos}/>
+                    <Tabla hasCheckboxes={true} setSelectedVars={setSelectedVars} filteredData={dataCsv} setFilteredData={setDataCsv} atributos={atributos} emptiedFilters={emptiedFilters} setEmptiedFilters={setEmptiedFilters}/>
                     <input
                     className="rounded-lg w-2/6 py-1 px-3 text-center border-solid border-2"
                     placeholder="Modelo 1"
