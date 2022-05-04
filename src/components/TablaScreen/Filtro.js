b60c418f57f76045f9f17245c5a8cfd70659e582
adriabellak <adriabellak@gmail.com>
2022-05-03 22:11:23 -0500

    Añadir barras de búsqueda para filtros de tablas

--- src/components/TablaScreen/Filtro.js
+++ src/components/TablaScreen/Filtro.js
@@ -6,20 +6,20 @@ import "./Filtro.css"
 export const Filtro = (props) => {
     const atributo = props.atributo;
     const setFilteredData = props.filterFunction;
-    var valuesChecked = useRef([]);   
+    var valuesChecked = useRef([]);
 
     function getUniqueData(value, index, self) {
         return self.indexOf(value) === index;
     }
 
     function aplicarFiltro(valores) {
-        setFilteredData(prevData => 
-            prevData.filter((value,index) => {return valores.current.includes(value[atributo])})
+        setFilteredData(prevData =>
+            prevData.filter((value, index) => { return valores.current.includes(value[atributo]) })
         )
         props.filterAppliedFunction();
     }
 
-    var column = props.filteredData.map((value,index) => {return value[atributo]});
+    var column = props.filteredData.map((value, index) => { return value[atributo] });
     column = column.filter(getUniqueData);
 
     const [checkedState, setCheckedState] = useState(
@@ -28,28 +28,46 @@ export const Filtro = (props) => {
 
     function handleCheckboxChange(position, value) {
         const updatedCheckedState = checkedState.map((item, index) =>
-        index === position ? !item : item
+            index === position ? !item : item
         );
 
         setCheckedState(updatedCheckedState);
 
         if (updatedCheckedState[position]) {
             valuesChecked.current.push(value);
-        } 
+        }
         else {
-            valuesChecked.current.splice(valuesChecked.current.indexOf(value),1);
+            valuesChecked.current.splice(valuesChecked.current.indexOf(value), 1);
         }
     }
-    
-    return(
+
+    const [wordEntered, setWordEntered] = useState("");
+
+    const handleSearch = (event) => {
+        const searchWord = event.target.value;
+        setWordEntered(searchWord);
+    };
+
+    return (
         <div className="filterBox">
+            <form action="/" method="get" className='text-center pt-1'>
+                <input
+                    type="text"
+                    id="header-search"
+                    placeholder="Buscar"
+                    name="s"
+                    className='bg-[#F2F2F2] rounded-xl pt-2 pb-2 pl-3 w-90 h-2/3'
+                    value={wordEntered}
+                    onChange={handleSearch}
+                />
+            </form>
             <ul>
-                {column.map((value, index) =>
-                                <li>
-                                    <input type="checkbox" checked={checkedState[index]} onChange={() => {handleCheckboxChange(index, value)}}/>
-                                    {value}
-                                </li>
-                        )}
+                {column.filter(value => value.toString().toLowerCase().includes(wordEntered.toLowerCase())).map((value, index) =>
+                    <li>
+                        <input type="checkbox" checked={checkedState[index]} onChange={() => { handleCheckboxChange(index, value) }} />
+                        {value}
+                    </li>
+                )}
             </ul>
 
             <button className="btn btn-primary aplicar-filtro" onClick={() => aplicarFiltro(valuesChecked)}>Aplicar</button>
