c0b004d34fecf502511226d93772f07078eed973
adriabellak <adriabellak@gmail.com>
2022-05-18 02:53:10 -0500

    Arreglar busqueda de columnas

--- src/components/SubirArchivo/Relacionar/ContenedorRelacionar.js
+++ src/components/SubirArchivo/Relacionar/ContenedorRelacionar.js
@@ -1,73 +1,81 @@
-import { DragDropContext, Droppable, Draggable} from "react-beautiful-dnd"
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd"
 import BloqueAtributo from "./BloqueAtributo"
-import {useState} from "react"
+import { useState, useEffect } from "react"
 import BloqueSubido from "./BloqueSubido"
 import { Buscar } from "./Buscar"
 import { useOutletContext } from "react-router-dom";
 import { render } from "@testing-library/react"
+import "./Relacionar.css"
+
 
 
 const Contenedor = () => {
 
+    const [wordEntered, setWordEntered] = useState("");
+
+    const handleSearch = (event) => {
+        const searchWord = event.target.value;
+        setWordEntered(searchWord);
+    };
+
     const [file, setFile, dataCsv, setDataCsv, columnas, setColumnas, actInt, setActInt, actExt, setActExt, stepActual, setStepActual] = useOutletContext();
-    
+
     const [atributos, setAtributos] = useState(columnas);
 
     const onDragEnd = (result) => {
 
         // console.log(result)
-        const {source, destination} = result;
-        if(!destination) return;
+        const { source, destination } = result;
+        if (!destination) return;
 
-        if(destination.droppableId === source.droppableId && source.index === destination.index) return
-    
+        if (destination.droppableId === source.droppableId && source.index === destination.index) return
+
         let add, arregloExternos = actExt, subidos = atributos, arregloInternos = actInt;
-        
+
 
         //-------------SOURCE LOGIC------------------------
 
         //Para subidos
-        if(source.droppableId === "subidos"){
+        if (source.droppableId === "subidos") {
             add = subidos[source.index];
-            subidos.splice(source.index,1);
+            subidos.splice(source.index, 1);
         }
 
         //---------EXTERNOS------------
-        else if(source.droppableId === "externos"){
-                add = arregloExternos[source.index]
-                arregloExternos.splice(source.index,1)
+        else if (source.droppableId === "externos") {
+            add = arregloExternos[source.index]
+            arregloExternos.splice(source.index, 1)
         }
 
 
         //---------INTERNOS------------
-        else if(source.droppableId === "internos"){
+        else if (source.droppableId === "internos") {
             add = arregloInternos[source.index]
-            arregloInternos.splice(source.index,1)
+            arregloInternos.splice(source.index, 1)
         }
 
 
 
         //-------------DESTINATION LOGIC------------------------
-        if(destination.droppableId === "subidos"){
-            subidos.splice(destination.index,0,add)
+        if (destination.droppableId === "subidos") {
+            subidos.splice(destination.index, 0, add)
         }
-        else if(destination.droppableId === "externos"){
-            arregloExternos.splice(destination.index,0,add)
+        else if (destination.droppableId === "externos") {
+            arregloExternos.splice(destination.index, 0, add)
         }
 
         //-----------INTERNOS------------
 
-        else if(destination.droppableId === "internos"){
-            arregloInternos.splice(destination.index,0,add)
+        else if (destination.droppableId === "internos") {
+            arregloInternos.splice(destination.index, 0, add)
         }
 
 
 
-        
+
         setAtributos(subidos)
         setActExt(arregloExternos)
         setActInt(arregloInternos)
-        
     }
 
 
@@ -78,32 +86,37 @@ const Contenedor = () => {
                 {/* Columna de actores externos */}
                 <div className="columna">
                     <h5>Actores externos</h5>
-                    <BloqueAtributo titulo="Planta Transportista" color="backgroundNaranjaClaro" id="externos" arregloAtributos ={actExt}/>
+                    <BloqueAtributo titulo="Planta Transportista" color="backgroundNaranjaClaro" id="externos" arregloAtributos={actExt} />
                 </div>
 
                 <div className="columna">
 
-                <h5 className="centrarTexto">Columnas del archivo subido</h5>
-                <Buscar/>
+                    <h5 className="centrarTexto">Columnas del archivo subido</h5>
+                    {/* <Buscar/> */}
+                    <div className="buscar">
+                        <div className="input-group rounded">
+                            <input type="search" className="form-control rounded" placeholder="Buscar" aria-label="Search" aria-describedby="search-addon" value={wordEntered} onChange={handleSearch}/>
+                        </div>
+                    </div>
                     <Droppable droppableId="subidos">
-                        { (droppableProvided) => (
-                        <ul className="columnaMedia overflow-auto"
-                            {...droppableProvided.droppableProps}
-                            ref={droppableProvided.innerRef}
-                        >
-                            {atributos.map((atributo, index) => (
-                                <Draggable key={atributo} draggableId={atributo} index={index}>
-                                    {(draggableProvided) => (
-                                        <li
-                                        {...draggableProvided.draggableProps}
-                                        ref={draggableProvided.innerRef}
-                                        {...draggableProvided.dragHandleProps}
-                                        ><BloqueSubido nombreAtributo={atributo} espacio=" containerSubido"/></li>
-                                    )}
-                                </Draggable>
-                            ))}
-                            {droppableProvided.placeholder}
-                        </ul>
+                        {(droppableProvided) => (
+                            <ul className="columnaMedia overflow-auto"
+                                {...droppableProvided.droppableProps}
+                                ref={droppableProvided.innerRef}
+                            >
+                                {atributos.filter(value => value.toString().toLowerCase().includes(wordEntered.toLowerCase())).map((atributo) => (
+                                    <Draggable key={atributo} draggableId={atributo} index={atributos.findIndex((a) => a === atributo)}>
+                                        {(draggableProvided) => (
+                                            <li
+                                                {...draggableProvided.draggableProps}
+                                                ref={draggableProvided.innerRef}
+                                                {...draggableProvided.dragHandleProps}
+                                            ><BloqueSubido nombreAtributo={atributo} espacio=" containerSubido" /></li>
+                                        )}
+                                    </Draggable>
+                                ))}
+                                {droppableProvided.placeholder}
+                            </ul>
                         )}
                     </Droppable>
                 </div>
@@ -111,7 +124,7 @@ const Contenedor = () => {
 
                 <div className="columna">
                     <h5 className="alinearDerecha">Actores internos</h5>
-                    <BloqueAtributo titulo="Planta Transportista" color="backgroundAmarilloClaro" id="internos" arregloAtributos={actInt}/>
+                    <BloqueAtributo titulo="Planta Transportista" color="backgroundAmarilloClaro" id="internos" arregloAtributos={actInt} />
                 </div>
             </div>
         </DragDropContext>
