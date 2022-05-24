3d9781344139e580cba89f46d446401d12b07386
DessTEC <A01733753@itesm.mx>
2022-05-24 18:58:24 -0500

    Zoom y pan después de filtros

--- src/components/GraficasScreen/AddGraph.js
+++ src/components/GraficasScreen/AddGraph.js
@@ -28,6 +28,7 @@ import { createArrayBarrasCorrGen } from "../utils/createArrayBarrasCorrGen";
 
 import {createArrayForChart, createConf} from '../utils/configChart'
 import createDataForChart from '../utils/createDataForChart'
+import createOptionsCharts from "../utils/createOptionsCharts";
 
 Chart.register(ChartDataLabels);
 Chart.register(zoomPlugin);
@@ -64,134 +65,6 @@ export default function AddGraph(props) {
     );
    
 }
-
-
-
-  const optionsCharts = {
-    barras: {
-      responsive: true,
-      maintainAspectRatio: true,
-      plugins: {
-        datalabels: {
-          display: false,
-        },
-        zoom: {
-          pan: {
-            enabled: true,
-            mode: "xy"
-          },
-          zoom: {
-            enabled: true,
-            mode: "xy",
-            wheel: {
-              enabled: true
-            }
-          },
-        }
-      },
-      scales: {
-        x: {
-          stacked: true,
-          title: {
-            display:true,
-            text: "Variable",
-            font: {
-              size: 14
-            }
-          }
-        },
-        y: {
-          stacked: true,
-          title: {
-            display:true,
-            text: "Total de datos",
-            font: {
-              size: 14
-            }
-          }
-        },
-      },
-    },
-    dona: {
-      responsive: true,
-      maintainAspectRatio: false,
-      plugins: {
-        title: {
-          display: true,
-          text: analysisType === "Anomalías" ? `Anomalías ${varX}`: analysisType === "Correlación Puntual" ? `Anomalías ${varX} - ${varY}:${valueY}` : `Anomalías x:${varX} y:${varY}`,
-        },
-        datalabels: {
-          display: true,
-          align: 'center',
-          color: '#4B4B4B',
-          borderRadius: 3,
-          font: {
-            size: 18,
-          }
-        },
-      },
-    },
-    burbuja: {
-      responsive: true,
-      maintainAspectRatio: false,
-      scales: {
-        x: {
-          title: {
-            display:true,
-            text: "",
-            font: {
-              size: 14
-            }
-          }
-        },
-        y: {
-          title: {
-            display:true,
-            text: "",
-            font: {
-              size: 14
-            }
-          } 
-        },
-      },
-      plugins: {
-        datalabels: {
-          display: false,
-        },
-        zoom: {
-          pan: {
-            enabled: true,
-            mode: "xy"
-          },
-          zoom: {
-            enabled: true,
-            mode: "xy",
-            wheel: {
-              enabled: true
-            }
-          },
-        },
-        tooltip: {
-            callbacks: {
-                label: function(context) {
-                    let label = context.dataset.label || ''; //Obtener label de contexto
-
-                    if (label) {
-                        label += ': '; //Agregar dos puntos
-                    }
-                    if (context.parsed._custom !== null) { //Si el radio no es nulo
-                        label += context.raw.realR //Acceder a la información raw y obtener el valor del radio sin normalizar
-
-                        //Añadir coordenadas de burbuja
-                        label += ` x: ${context.raw.x} y: ${context.raw.y}`;
-                    }
-                    return label;
-                }
-            }
-        }
-      }
-    }
-  }
 
   const handleClick = () => {
     setIsMenuOpen(true);
@@ -240,6 +113,8 @@ export default function AddGraph(props) {
   const handleCreateChart = () => {
     setIsMenuOpen(false);
     const id = _uniqueId('id-');
+
+    const optionsCharts = createOptionsCharts(analysisType, varX, varY, valueY);
 
     let arrayForChart = createArrayForChart(props.dataModelo, analysisType, chartType, varX, varY, valueY, minValAnomalias, maxValAnomalias);
 
